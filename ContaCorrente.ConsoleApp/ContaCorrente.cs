d09226431f76ecaa613b9c87c910ea690ddf2376
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-04-06 16:08:42 -0300

    Melhorados comentários e apresentação

--- ContaCorrente.ConsoleApp/ContaCorrente.cs
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -1,11 +1,11 @@
 /*
-    Pilar da programação orientada à objetos -> Abstração
+    Abstração da Conta Corrente
 
-    (Descrição/Atributos da Conta Corrente) [x]
+    (Descrição/Atributos) [x]
     Uma conta corrente é composta por diversos atributos essenciais para sua operação.
     Ela possui um número de identificação único, um saldo disponível e um limite de débito estabelecido.
 
-    (Ações na/da Conta Corrente)
+    (Ações/Métodos) [x]
     Saque: Permite a retirada de valores, respeitando o limite máximo permitido.
     Depósito: Possibilita a adição de fundos à conta.
     Consulta de saldo: Fornece informações atualizadas sobre o montante disponível.
@@ -20,46 +20,64 @@ class ContaCorrente
 
     public void Sacar()
     {
+        Console.WriteLine("-------------------------------------");
         Console.Write("Digite o valor que deseja sacar (R$): ");
         decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
 
         if (saldo <= -limiteDebito)
         {
+            Console.WriteLine("-------------------------------------");
             Console.WriteLine("O valor do limite de débito já foi ultrapassado!");
             Console.ReadLine();
         }
         else
         {
             saldo -= valorSaque;
-
+            Console.WriteLine("-------------------------------------");
             Console.WriteLine("O valor foi sacado com sucesso!");
+            Console.WriteLine("-------------------------------------");
+            Console.Write("Digite ENTER para continuar...");
             Console.ReadLine();
         }
     }
 
     public void Depositar()
     {
+        Console.WriteLine("-------------------------------------");
         Console.Write("Digite o valor que deseja depositar (R$): ");
         decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
 
         saldo += valorDeposito;
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("O valor foi sacado com sucesso!");
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Digite ENTER para continuar...");
+        Console.ReadLine();
     }
 
     public void TransferirPara(ContaCorrente contaDestino)
     {
+        Console.WriteLine("-------------------------------------");
         Console.Write("Digite o valor que deseja transferir (R$): ");
         decimal valorTransferencia = Convert.ToDecimal(Console.ReadLine());
 
         saldo -= valorTransferencia;
         contaDestino.saldo += valorTransferencia;
 
+        Console.WriteLine("-------------------------------------");
         Console.WriteLine($"O valor de R${valorTransferencia} foi tranferido com sucesso!");
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Digite ENTER para continuar...");
         Console.ReadLine();
     }
 
     public void ObterSaldo()
     {
+        Console.WriteLine("-------------------------------------");
         Console.WriteLine("O valor do saldo da conta é de (R$): " + saldo);
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Digite ENTER para continuar...");
         Console.ReadLine();
     }
 }
--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,19 +1,4 @@
-﻿/*
-    Pilar da programação orientada à objetos -> Abstração
-
-    (Descrição/Atributos da Conta Corrente) [x]
-    Uma conta corrente é composta por diversos atributos essenciais para sua operação.
-    Ela possui um número de identificação único, um saldo disponível e um limite de débito estabelecido.
-
-    (Ações na/da Conta Corrente)
-    Saque: Permite a retirada de valores, respeitando o limite máximo permitido.
-    Depósito: Possibilita a adição de fundos à conta.
-    Consulta de saldo: Fornece informações atualizadas sobre o montante disponível.
-    Transferência entre contas: Permite a movimentação de valores entre contas correntes distintas.
-*/
-using System.Security.Cryptography;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
@@ -21,24 +6,29 @@ class Program
         ContaCorrente contaUm = new ContaCorrente();
         contaUm.numeroIdentificacao = 1;
         contaUm.titular = "Tiago";
+        contaUm.saldo = 400;
+        contaUm.limiteDebito = 1200;
 
         // Conta Corrente 2
         ContaCorrente contaDois = new ContaCorrente();
         contaDois.numeroIdentificacao = 2;
         contaDois.titular = "Rech";
         contaDois.saldo = 12000;
+        contaDois.limiteDebito = 1200;
 
         while (true)
         {
-            // Console.Clear();
-            Console.WriteLine("---------------------------------------------");
+            Console.Clear();
+            Console.WriteLine("-------------------------------------");
             Console.WriteLine($"Conta Corrente #{contaUm.numeroIdentificacao} de {contaUm.titular}");
-            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("-------------------------------------");
             Console.WriteLine("1 - Saque");
             Console.WriteLine("2 - Depósito");
             Console.WriteLine("3 - Transferência");
             Console.WriteLine("4 - Consulta de Saldo");
             Console.WriteLine("S - Sair");
+            Console.WriteLine("-------------------------------------");
+            Console.Write("Digite uma opção válida: ");
             string? opcaoMenu = Console.ReadLine()?.ToUpper();
 
             if (opcaoMenu == "S")
@@ -54,7 +44,7 @@ class Program
             }
             else if (opcaoMenu == "3")
             {
-                contaUm.TransferirPara(contaDois); // passa o endereço da variável
+                contaUm.TransferirPara(contaDois);
             }
             else if (opcaoMenu == "4")
             {
